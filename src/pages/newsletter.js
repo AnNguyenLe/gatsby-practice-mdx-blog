739dcf0f544bdb9925d071917e815019a2f6ed73
An Nguyen Le Thien <an@inspectorio.com>
2022-05-11 17:37:18 +0700

    building frame for new blog

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -2,7 +2,40 @@ import React from 'react'
 import Layout from '../components/Layout'
 const NewsLetter = () => {
   return (
-    <h4>newsletter</h4>
+    <Layout>
+      <section className="newsletter-page">
+        <div className="page-center">
+          <h2>Get all the latest stories to your inbox</h2>
+          <h4>I write to my friends every fews weeks</h4>
+          <form
+            className="contact-form"
+            name="testing-contact"
+            method="post"
+            netlify-honeypot="bot-field"
+            data-netlify="true"
+            action="/success"
+          >
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="testing-contact" />
+            <input
+              className="form-control"
+              type="text"
+              name="name"
+              placeholder="Your name"
+            />
+            <input
+              className="form-control"
+              type="email"
+              name="email"
+              placeholder="Your email"
+            />
+            <button type="submit" className="btn form-control submit-btn">
+              subscribe
+            </button>
+          </form>
+        </div>
+      </section>
+    </Layout>
   )
 }
 
